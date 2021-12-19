9f2c60a21179241a0e9885895713c97def592978
Mihail Minkov <Mihail.Minkov.BG@gmail.com>
2021-12-19 14:41:25 +0200

    optimize 2021-19

--- 2021/19/app.py
+++ 2021/19/app.py
@@ -1,43 +1,21 @@
 #!/usr/bin/env python
 #-*- coding: UTF-8 -*-
 
-import json
-import pprint
+import sys
+import queue
+
+permutations = (( 0, 1, 2 ), ( 0, 2, 1 ), ( 1, 0, 2 ), ( 1, 2, 0 ), ( 2, 0, 1 ), ( 2, 1, 0 ))
 
 
-def parse_beacon(row):
-    return tuple(map(int, row.split(',')))
-
-
-def parse_scanner(data):
-    rows = data.split("\n")
-    return tuple(map(parse_beacon, rows[1:]))
-
-
-def parse_input(data):
-    return tuple(map(parse_scanner, data.split("\n\n")))
-
-
-permutations = ( 
-    ( 0, 1, 2 ),
-    ( 0, 2, 1 ),
-    ( 1, 0, 2 ),
-    ( 1, 2, 0 ),
-    ( 2, 0, 1 ),
-    ( 2, 1, 0 ),
- )
+def parse_beacon(row): return tuple(map(int, row.split(',')))
+def parse_scanner(data): return tuple(map(parse_beacon, data.split("\n")[1:]))
+def parse_input(data): return list(map(parse_scanner, data.split("\n\n")))
 
 
 def get_rotations(beacons):
-    result = []
-    for beacon in beacons:
-        result.append(get_rotations_single(beacon))
-
-    l = len(result[0])
-    ret = []
-    for i in range(l):
-        ret.append(tuple(row[i] for row in result))
-    return ret
+    result = tuple(get_rotations_single(beacon) for beacon in beacons)
+    return tuple(tuple(row[i] for row in result)
+        for i in range(len(result[0])))
 
 
 def get_rotations_single(pos):
@@ -59,142 +37,113 @@ def get_rotations_single(pos):
                 pos_1[1] * flip1,
                 pos_1[2] * flip2,
             )
-            # print flip, flip0, flip1, flip2, pos_2
             results.append(pos_2)
     return results
 
 
-def common(r1, r2):
-    return len(set(r1).intersection(r2))
-
-def process(beacons):
-    beacons = tuple(sorted(beacons))
-    m = beacons[0]
-    beacons = ((a[0] - m[0], a[1] - m[1], a[2] - m[2])
-        for a in beacons)
-    beacons = tuple(beacons)
-    return beacons, m
+def subtract_point(p1, p2):
+    return (p1[0] - p2[0], p1[1] - p2[1], p1[2] - p2[2])
 
 
-def find_common_beacons(scanner_a, scanner_b, limit = 12):
-    rot2 = get_rotations(scanner_b)
+def add_point(p1, p2):
+    return (p1[0] + p2[0], p1[1] + p2[1], p1[2] + p2[2])
 
-    for b1 in scanner_a:
-        for r2 in rot2:
-            for b2 in r2:
+
+def find_common_beacons(beacons_a, beacons_b, limit = 12):
+    # scanner_a has a position,
+    # scanner_b does not have a position
+    # scanner_a coordinates are absolute
+    # scanner_b coordinates are relative
+
+    beacons_a = set(beacons_a)
+    for beacons_rotated in get_rotations(beacons_b):
+        for b2 in beacons_rotated:
+            for b1 in beacons_a:
                 # assume b1 and b2 are the same.
                 # Then check if there's enough of a match
-                m = (b2[0] - b1[0], b2[1] - b1[1], b2[2] - b1[2])
-                r2p = tuple((a[0] - m[0], a[1] - m[1], a[2] - m[2])
-                    for a in r2)
+                offset = subtract_point(b1, b2)
+                beacons_rotated_absolute = tuple(add_point(a, offset) for a in beacons_rotated)
+                common = beacons_a.union(beacons_rotated_absolute)
 
-                c = common(scanner_a, r2p)
-                if c >= limit:
-                    common_beacons = tuple(sorted(set(scanner_a + r2p)))
-                    return common_beacons, m
+                if len(beacons_a) + len(beacons_rotated) - len(common) >= limit:
+                    return beacons_rotated_absolute, offset
+                    # This is an order of magnitude slower, but it's actually correct:
+                    # return common, offset
+
+                    # Note: the "Real" solution would require us to merge the points
+                    # every time we join two regions.
+                    # Suppose we have regions A, B, C
+                    # Each of the regions A and B might not have 12 common points with C
+                    # - but the union A+B might have them.
+
+                    # Example:
+                    # Clearly, A & B have 12 common points and can be merged
+                    # However, C doesn't have 12 common points with either A or B.
+                    # A merge cannot happen
+                    # But, C has 12 common points with the union (A+B).
+                    # Therefore, in the general case we would have to merge every 2 scanners that we match
+                    #            ---------------------
+                    #            |        C          |
+                    #            |                   |
+                    #            |                   |
+                    #            |                   |
+                    #     ---------------------      |     
+                    #     |      |  ....  |----------|--------|
+                    #     |      |  ....  |   | .... |        |
+                    #     |      --------------------|        |
+                    #     |               |...|               |
+                    #     |  A            |...|            B  |
+                    #     |               |...|               |
+                    #     |               |...|               |
+                    #     |               |   |               |
+                    #     ----------------|----               |
+                    #                     ---------------------
+                    # return common, offset
 
 
 def max_offset(offsets):
-    relative_offsets = []
-    for i in range(len(offsets)):
-        oa = offsets[i]
-        distance = sum(map(abs, oa))
-        # print distance, oa
-        relative_offsets.append(distance)
-
-    for i in range(len(offsets)):
-        for j in range(i+1, len(offsets)):
-            ob = offsets[j]
-            ro = ((oa[0] - ob[0]), (oa[1] - ob[1]), (oa[2] - ob[2]))
-            distance = sum(map(abs, ro))
-            # print distance, ro, oa, ob
-            relative_offsets.append(distance)
-    return max(relative_offsets)
+    return max(
+        # get all relative offsets between each pair of scanners
+        sum(map(abs, subtract_point(oa, ob)))
+        for oa in offsets
+        for ob in offsets
+        if oa != ob
+    )
 
 
 def solve(data):
-    data = parse_input(data)
-    cycle = 0
-    def optimize(data):
-        # print 'optimize', len(data)
-        for j in range(1, len(data)):
-            print j
-            result = find_common_beacons(data[0], data[j])
-            if result:
-                beacons, offset = result
-                data = [row for x, row in enumerate(data) if x != 0 and x != j]
-                data.insert(0, beacons)
-                # print j, len(data), offset
-                # print len(data), offset, len(data[0]), [len(i) for i in data[:4]]
-                return data, offset
+    beacons = parse_input(data)
+    scanners = [None] * len(beacons) # scanners have no fixed positions at first
+    scanners[0] = (0, 0, 0) # The first scanner is assumed to be at (0, 0, 0)
 
+    q = queue.Queue()
+    q.put(0)
+    while not q.empty():
+        i = q.get()
+        for j in range(len(beacons)):
+            if i == j or scanners[j]: continue
+            result = find_common_beacons(beacons[i], beacons[j])
+            if not result: continue
+            beacons[j] = result[0]
+            scanners[j] = result[1]
+            print >>sys.stderr, "found match:", (i, j), sum(map(bool, scanners))
+            q.put(j)
 
-    offsets = []
-    while len(data) > 1:
-        data, offset = optimize(data)
-        cycle += 1
-        offsets.append(offset)
-        # print len(data), offset
-        # print offset
-        # print '--'
-        print len(data), len(data[0]), max_offset(offsets)
-
-    # print len(data[0])
-    # print max_offset(offsets)
-    return len(data[0]), max_offset(offsets)
-
-
-def easy(data):
-    return solve(data)[0]
-
-
-def hard(data):
-    return solve(data)[1]
+    beacons = set(sum(beacons, tuple()))
+    return len(beacons), max_offset(scanners)
 
 
 def test():
-    beacons = open('in_test_simple.txt').read()
-    beacons = parse_input(beacons)
-    rotations = get_rotations(beacons[0])
-    assert beacons[0] in rotations
-    assert beacons[1] in rotations
-    assert beacons[2] in rotations
-    assert beacons[3] in rotations
-    assert beacons[4] in rotations
-
-    # pprint.pprint(len(rotations))
-    # pprint.pprint(len(set(rotations)))
-    # pprint.pprint(len(rotations))
-    pprint.pprint(rotations[0][0])
-
     data = open('in_test.txt').read()
-    data = parse_input(data)
-    offsets = []
-    beacons, offset = find_common_beacons(data[0], data[1])
-    offsets.append(offset)
-    print len(beacons), offset
-    beacons, offset = find_common_beacons(beacons, data[4])
-    offsets.append(offset)
-    print len(beacons), offset
-    beacons, offset = find_common_beacons(beacons, data[2])
-    offsets.append(offset)
-    print len(beacons), offset
-    beacons, offset = find_common_beacons(beacons, data[3])
-    offsets.append(offset)
-    print len(beacons), offset
-    print max_offset(offsets)
-
-    data = open('in_test.txt').read()
-    # assert easy(data) == 79
-    assert hard(data) == 3621
+    assert solve(data) == (79, 3621)
 
 
 def main():
     test()
-    # exit()
     data = open('in.txt').read()
-    print easy(data)
-    # print hard(data)
+    answer_easy, answer_hard = solve(data)
+    print answer_easy
+    print answer_hard
 
 
 if __name__ == '__main__':
