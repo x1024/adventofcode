b33b3cf980f7058853ad21e26bacec2770296b22
Mihail Minkov <Mihail.Minkov.BG@gmail.com>
2022-01-24 14:27:56 +0200

    Add 2019/18

--- 2019/src/bin/18.rs
+++ 2019/src/bin/18.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
-use num::abs;
-use queues::*;
+use queues::{IsQueue, Queue};
 use std::fs;
 
 #[cfg(test)]
@@ -68,6 +67,7 @@ pub mod tests {
         assert_eq!(easy(&data), 81);
     }
 
+    #[test]
     fn test_hard_1() {
         let data = "#######
                     #a.#Cd#
@@ -80,6 +80,46 @@ pub mod tests {
         assert_eq!(hard(&data), 8);
     }
 
+    #[test]
+    fn test_hard_2() {
+        let data = "###############
+                    #d.ABC.#.....a#
+                    ######...######
+                    ######.@.######
+                    ######...######
+                    #b.....#.....c#
+                    ###############";
+        let data = parse_input(data);
+        assert_eq!(hard(&data), 24);
+    }
+
+    #[test]
+    fn test_hard_3() {
+        let data = "#############
+                    #DcBa.#.GhKl#
+                    #.###...#I###
+                    #e#d#.@.#j#k#
+                    ###C#...###J#
+                    #fEbA.#.FgHi#
+                    #############";
+        let data = parse_input(data);
+        assert_eq!(hard(&data), 32);
+    }
+
+    #[test]
+    fn test_hard_4() {
+        let data = "#############
+                    #g#f.D#..h#l#
+                    #F###e#E###.#
+                    #dCba...BcIJ#
+                    #####.@.#####
+                    #nK.L...G...#
+                    #M###N#H###.#
+                    #o#m..#i#jk.#
+                    #############";
+        let data = parse_input(data);
+        assert_eq!(hard(&data), 72);
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -95,32 +135,95 @@ type Point = (i64, i64);
 type Pos = (Point, Point, Point, Point);
 type Map = Vec<Vec<Tile>>;
 
-struct P18Easy {
+struct P18 {
     map: Map,
-    cache: HashMap<(Point, i64), Vec::<(i64, i64, Point)>>
+    cache_easy: HashMap<(Point, i64), i64>,
+    cache_hard: HashMap<(Pos, i64), i64>,
 }
 
-impl P18Easy {
-    fn new(map: &Map) -> P18Easy {
-        P18Easy { map: map.clone(), cache: HashMap::new() }
+impl P18 {
+    fn new(map: &Map) -> P18 {
+        P18 {
+            map: map.clone(),
+            cache_easy: HashMap::new(),
+            cache_hard: HashMap::new(),
+        }
     }
 
     fn solve_easy(&mut self, start: Point, unlocked: i64) -> i64 {
-        let to_check = self.dfs(start, unlocked);
+        if let Some(result) = self.cache_easy.get(&(start, unlocked)) {
+            return *result;
+        }
 
+        let to_check = self.dfs(start, unlocked);
         let result = to_check.iter()
             .map(|(steps, key, pos)| steps + self.solve_easy(*pos, unlocked | 1<<*key))
             .min()
             .unwrap_or(0);
 
+        self.cache_easy.insert((start, unlocked), result);
         result
     }
 
-    fn dfs(&mut self, start: Point, unlocked: i64) -> &Vec::<(i64, i64, Point)> {
-        if let Some(result) = self.cache.get(&(start, unlocked)) {
-            return result;
+    fn solve_hard(&mut self, start: Pos, unlocked: i64) -> i64 {
+        if let Some(result) = self.cache_hard.get(&(start, unlocked)) {
+            return *result;
         }
 
+        let mut results = Vec::<i64>::new();
+
+        let to_check_0 = self.dfs(start.0, unlocked);
+        let to_check_1 = self.dfs(start.1, unlocked);
+        let to_check_2 = self.dfs(start.2, unlocked);
+        let to_check_3 = self.dfs(start.3, unlocked);
+
+        if let Some(steps) = to_check_0.iter()
+            .map(|(steps, key, pos)| {
+                let new_pos = (*pos, start.1, start.2, start.3);
+                steps + self.solve_hard(new_pos, unlocked | 1<<*key)
+            })
+            .min() {
+            results.push(steps);
+        }
+
+        if let Some(steps) = to_check_1.iter()
+            .map(|(steps, key, pos)| {
+                let new_pos = (start.0, *pos, start.2, start.3);
+                steps + self.solve_hard(new_pos, unlocked | 1<<*key)
+            })
+            .min() {
+            results.push(steps);
+        }
+
+        if let Some(steps) = to_check_2.iter()
+            .map(|(steps, key, pos)| {
+                let new_pos = (start.0, start.1, *pos, start.3);
+                steps + self.solve_hard(new_pos, unlocked | 1<<*key)
+            })
+            .min() {
+            results.push(steps);
+        }
+
+        if let Some(steps) = to_check_3.iter()
+            .map(|(steps, key, pos)| {
+                let new_pos = (start.0, start.1, start.2, *pos);
+                steps + self.solve_hard(new_pos, unlocked | 1<<*key)
+            })
+            .min() {
+            results.push(steps);
+        }
+
+        let result = if results.len() > 0 {
+            *results.iter().min().unwrap()
+        } else {
+            0
+        };
+
+        self.cache_hard.insert((start, unlocked), result);
+        result
+    }
+
+    fn dfs(&mut self, start: Point, unlocked: i64) -> Vec::<(i64, i64, Point)> {
         // println!("DFS: {:?} {}", start, unlocked);
         let mut q = Queue::<(i64, Point)>::new();
         let map = &self.map;
@@ -167,23 +270,13 @@ impl P18Easy {
             }
         }
 
-        if unlocked.count_ones() < 4{
-            println!("{:?} {} {:?}", start, unlocked, to_check.len());
-        }
-
-        self.cache.insert((start, unlocked), to_check.clone());
-        return self.cache.get(&(start, unlocked));
-    }
-
-    fn dfs_multi(&mut self, start: Pos, unlocked: i64) -> i64 {
         /*
-        to_check_a
-        to_check_b
-        to_check_c
-        to_check_d
+        if unlocked.count_ones() < 4 {
+            println!("{:?} {} {} {:?}", start, unlocked, unlocked.count_ones(), to_check.len());
+        }
         */
 
-        0
+        to_check
     }
 }
 
@@ -218,18 +311,43 @@ fn easy(map: &Map) -> i64 {
         }
     }
 
-    let mut p18 = P18Easy::new(map);
+    let mut p18 = P18::new(map);
     p18.solve_easy(player_pos, 0)
 }
 
 fn hard(map: &Map) -> i64 {
-0
+    let mut map = map.clone();
+    for row in 0..map.len() {
+        for col in 0..map[0].len() {
+            if let Tile::Player = map[row][col] {
+                let pos = (row, col);
+                map[pos.0+0][pos.1+0] = Tile::Wall;
+                map[pos.0+1][pos.1+0] = Tile::Wall;
+                map[pos.0-1][pos.1+0] = Tile::Wall;
+                map[pos.0+0][pos.1+1] = Tile::Wall;
+                map[pos.0+0][pos.1-1] = Tile::Wall;
+                let pos = (row as i64, col as i64);
+
+                let positions = (
+                    (pos.0+1, pos.1+1),
+                    (pos.0-1, pos.1+1),
+                    (pos.0+1, pos.1-1),
+                    (pos.0-1, pos.1-1)
+                );
+
+                let mut p18 = P18::new(&map);
+                return p18.solve_hard(positions, 0);
+            }
+        }
+    }
+
+    -1
 }
 
 fn main() {
     let input = fs::read_to_string("input/18.txt").expect("Unable to read input file");
     let input = parse_input(&input);
     println!("{}", easy(&input));
-    // println!("{}", hard(&input));
+    println!("{}", hard(&input));
     ()
 }
