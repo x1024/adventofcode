76f1d4463f5df9d1c3d32c649f38c5ace90584bb
Mihail Minkov <Mihail.Minkov.BG@gmail.com>
2022-09-22 21:24:12 +0300

    Add 2017-17

--- 2017/src/p17.rs
+++ 2017/src/p17.rs
@@ -10,25 +10,44 @@ mod tests {
     }
 
     #[test]
-    fn test_hard() {
+    fn test_easy_2() {
+        assert_eq!(solve(3, 10, 10), 4);
     }
 }
 
-pub fn easy(step: i32) -> i32 {
-    unimplemented!();
+pub fn solve(step: usize, limit: usize, result_index: usize) -> usize {
+    let mut next = Vec::<usize>::new();
+    next.resize(limit, 0);
+    let mut index = 0;
+    next.insert(0, 0);
+
+    for value in 1..limit+1 {
+        if value % 10000 == 0 {
+            println!("{}", value);
+        }
+        for _ in 0..step {
+            index = next[index];
+        }
+        let n = next[index];
+        next[index] = value;
+        next[value] = n;
+        index = value;
+    }
+
+    next[result_index]
 }
 
-pub fn hard(_data: &str) -> i32 {
-    unimplemented!();
+pub fn easy(step: usize) -> usize {
+    solve(step, 2017, 2017)
 }
 
-fn parse_input(_data: &str) -> &str {
-    unimplemented!();
+pub fn hard(step: usize) -> usize {
+    solve(step, 50 * 1000 * 1000, 0)
 }
 
 pub fn main() {
     let input = fs::read_to_string("input/17.txt").unwrap();
-    let input = parse_input(&input);
-    println!("{}", easy(&input));
-    println!("{}", hard(&input));
+    let input = input.parse::<usize>().unwrap();
+    println!("{}", easy(input));
+    println!("{}", hard(input));
 }
